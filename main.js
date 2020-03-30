094d74366d82eb4fc1138a43b8cbc7ec80423202
Tianna <tianna.hydro@csulb.edu>
2020-03-29 23:42:20 -0700

    f

---
+++ main.js
@@ -0,0 +1,39 @@
+
+function numberValidate() {
+     const start = Number(prompt('Enter the start number: '));
+     if (start != NaN) {
+          const end = Number(prompt('Enter the end number: '));
+          if (end != NaN) {
+               const step = Number(prompt('Enter the step value: '));
+               if (step != NaN && step != 0) {
+                    const result = createArray(start, end, step);
+                     // 
+                    return result;
+               }
+          }
+     } 
+     else {
+          const result = "Invalid integer input, please reload and try again.";
+          return result;
+     } 
+}
+
+function createArray(start, end, step) {
+     values = [];
+     if (start < end) {
+          for (let i = start; i <= end; i += step) {
+               values.push(i);
+          }
+     }
+     else {
+          for (let i = start; i >= end; i += step) {
+               values.push(i);
+          }  
+     }
+     const result = `The generated array is ${values}`;
+     return result;   
+}
+
+const outputStr = numberValidate();
+const outputDiv = document.getElementById("output");
+outputDiv.innerHTML = "<h3>" + outputStr + "</h3>";
